b82412e2c484dcc2285b88cbbc929f6ac9c180f9
Александр Овчинников <alex-ovchinnikov@mail.ru>
2023-09-18 21:31:00 +0300

    before second review

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,13 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import F
-from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from recipes.models import Ingredient, IngredientInRecipe, Recipe, Tag
 from rest_framework import serializers, status
 from rest_framework.exceptions import ValidationError
-from rest_framework.fields import IntegerField, SerializerMethodField
+from rest_framework.fields import SerializerMethodField
 from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.serializers import ModelSerializer
 from users.models import Follow
